2a910545b9c1ee92ae24dffaeabfc0726d6065d0
Reese Wilson <reese@shinymayhem.com>
2015-03-17 15:43:21 -0700

    fixing auto-fetch from repo

--- node/get-code.sh
+++ node/get-code.sh
@@ -7,16 +7,24 @@ then
 	chown -R reese:reese ./
 	if [ `find ./ -prune -empty` ]
 	then
-		sudo -u reese -H sh -c "
-		git init;
-		git remote add origin $REPO;
-		git fetch;
-		git merge origin/master;
-		NPM_DIR=$(find ./ -maxdepth 2 -name `package.json` | xargs dirname);
-		cd NPM_DIR;
-		npm install;
-		"
+		#sudo -u reese -H sh -c "
+		sudo -u reese /bin/sh <<-token
+			cd $CODE_DIR
+			git init
+			git remote add origin $REPO
+			git fetch
+			git merge origin/master
+			echo "Fetched and merged"
+		token
+		NPM_DIR=$(find ./ -maxdepth 2 -name 'package.json' | xargs dirname)
+		cd $NPM_DIR
+		echo $(pwd)
+		npm install
+		#"
+	else
+		echo "Directory not empty. Leaving it alone \n";
 	fi
 else
 	echo "Please set CODE_DIR environment variable if you want to initialize a directory with code from a repository \n"
 fi
+echo "finished \n"
